49275fa5843a45bc2967cadd5563ebd1c3d5b08b
Luis <luis.p.melgar@disney.com>
2019-06-28 16:53:28 -0700

    play music by genre

--- MusicPlayer/GenreButtonViewController.swift
+++ MusicPlayer/GenreButtonViewController.swift
@@ -8,20 +8,43 @@
 
 import UIKit
 import AudioToolbox
+import MediaPlayer
 
 class GenreButtonViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
     
     @IBAction func genreButtonTapped(_ sender: UIButton) {
-        
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                 self.playGenre(genre: sender.currentTitle!)
+            }
+        }
     }
     
-//    @IBAction func Button1_Click(_ sender: Any) {
-//        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-//    }
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        //vibration test
+        //AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String) {
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+        
+    }
 }
